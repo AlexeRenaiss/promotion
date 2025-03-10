3e1c589127d8129870cb1fa74ee0078fa88a8960
NitinSinghElegant <130339696+NitinSinghElegant@users.noreply.github.com>
2025-03-10 12:29:01 +0530

    change button width

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const CustomButton = ({ onClick }) => {
   return (
     <button
-      className="w-1/4 max-w-xs bg-primary text-white font-medium py-3 px-4 rounded-full shadow-lg transition hover:bg-primary text-xs tracking-wider"
+      className="w-2/6 max-w-xs bg-primary text-white font-medium py-3 px-4 rounded-full shadow-lg transition hover:bg-primary text-xs tracking-wider"
       onClick={onClick}
     >
       VIEW ALL
