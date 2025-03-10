69e99e30126d90f8d74175d24e36681949ca11a4
NitinSinghElegant <130339696+NitinSinghElegant@users.noreply.github.com>
2025-03-10 11:28:55 +0530

    add fonts

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-const CustomButton = () => {
+const CustomButton = ({ onClick }) => {
   return (
-    <button className="w-1/4 max-w-xs bg-primary text-white font-medium py-3 px-4 rounded-full shadow-lg transition hover:bg-primary text-xs tracking-wider">
+    <button
+      className="w-1/4 max-w-xs bg-primary text-white font-medium py-3 px-4 rounded-full shadow-lg transition hover:bg-primary text-xs tracking-wider"
+      onClick={onClick}
+    >
       VIEW ALL
     </button>
   );
--- src/components/RewardSection.tsx
+++ src/components/RewardSection.tsx
@@ -1,7 +1,10 @@
+"use client";
 import Image from "next/image";
 import CustomButton from "./Button";
+import React from "react";
 
 export default function RewardSection() {
+  const [showAll, setShowAll] = React.useState(false);
   return (
     <div className="max-w-6xl mx-auto px-4 py-12 my-8 ">
       <div className="text-center mb-8">
@@ -62,10 +65,114 @@ export default function RewardSection() {
             <div>Family Care Favorites Bundle</div>
           </div>
         </div>
+        {showAll && (
+          <>
+            {/* Reward Card 1 */}
+            <div className="bg-white rounded-lg shadow-sm overflow-hidden">
+              <div className="h-48 bg-blue-500 relative m-2 rounded-lg overflow-hidden">
+                <Image
+                  src="/images/reward1.png"
+                  alt="Health Care Favorites Bundle"
+                  width={384}
+                  height={192}
+                  className="w-full h-full object-cover rounded-lg"
+                />
+              </div>
+              <div className="p-4 text-center font-medium text-black">
+                <div className="mb-1 ">25 Points</div>
+                <div>Monthly Sweepstakes</div>
+              </div>
+            </div>
+
+            {/* Reward Card 2 */}
+            <div className="bg-white rounded-lg shadow-sm overflow-hidden">
+              <div className="h-48 bg-blue-500 relative m-2 rounded-lg overflow-hidden">
+                <Image
+                  src="/images/reward2.png"
+                  alt="Health Care Favorites Bundle"
+                  width={384}
+                  height={192}
+                  className="w-full h-full object-cover rounded-lg"
+                />
+              </div>
+              <div className="p-4 text-center font-medium text-black">
+                <div className="mb-1">25 Points</div>
+                <div>Family Care Favorites Bundle</div>
+              </div>
+            </div>
+
+            {/* Reward Card 3 */}
+            <div className="bg-white rounded-lg shadow-sm overflow-hidden">
+              <div className="h-48 bg-blue-500 relative m-2 rounded-lg overflow-hidden">
+                <Image
+                  src="/images/reward3.png"
+                  alt="Health Care Favorites Bundle"
+                  width={384}
+                  height={192}
+                  className="w-full h-full object-cover rounded-lg"
+                />
+              </div>
+              <div className="p-4 text-center font-medium text-black">
+                <div className="mb-1">25 Points</div>
+                <div>Family Care Favorites Bundle</div>
+              </div>
+            </div>
+            {/* Reward Card 1 */}
+            <div className="bg-white rounded-lg shadow-sm overflow-hidden">
+              <div className="h-48 bg-blue-500 relative m-2 rounded-lg overflow-hidden">
+                <Image
+                  src="/images/reward1.png"
+                  alt="Health Care Favorites Bundle"
+                  width={384}
+                  height={192}
+                  className="w-full h-full object-cover rounded-lg"
+                />
+              </div>
+              <div className="p-4 text-center font-medium text-black">
+                <div className="mb-1 ">25 Points</div>
+                <div>Monthly Sweepstakes</div>
+              </div>
+            </div>
+
+            {/* Reward Card 2 */}
+            <div className="bg-white rounded-lg shadow-sm overflow-hidden">
+              <div className="h-48 bg-blue-500 relative m-2 rounded-lg overflow-hidden">
+                <Image
+                  src="/images/reward2.png"
+                  alt="Health Care Favorites Bundle"
+                  width={384}
+                  height={192}
+                  className="w-full h-full object-cover rounded-lg"
+                />
+              </div>
+              <div className="p-4 text-center font-medium text-black">
+                <div className="mb-1">25 Points</div>
+                <div>Family Care Favorites Bundle</div>
+              </div>
+            </div>
+
+            {/* Reward Card 3 */}
+            <div className="bg-white rounded-lg shadow-sm overflow-hidden">
+              <div className="h-48 bg-blue-500 relative m-2 rounded-lg overflow-hidden">
+                <Image
+                  src="/images/reward3.png"
+                  alt="Health Care Favorites Bundle"
+                  width={384}
+                  height={192}
+                  className="w-full h-full object-cover rounded-lg"
+                />
+              </div>
+              <div className="p-4 text-center font-medium text-black">
+                <div className="mb-1">25 Points</div>
+                <div>Family Care Favorites Bundle</div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
 
       <div className="text-center mt-8">
-        <CustomButton />
+        <CustomButton onClick={() => setShowAll(!showAll)} />
       </div>
     </div>
   );
